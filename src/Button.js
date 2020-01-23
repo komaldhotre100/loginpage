db4a8ba217f243fbfb01763f78b22c9ac52d06d5
komaldhotre100 <komaldhotre7441@gmail.com>
2020-01-23 19:07:14 +0530

    initiak commit

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Button.css';
+
+function Button() {
+  return (
+      <input type='Button'/>
+    
+  );
+}
+
+export default Button;
